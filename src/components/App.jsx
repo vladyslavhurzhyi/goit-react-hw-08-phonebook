e9d918e34a08392e0ed3d3eb258a50336fde37fa
Vladyslav Hurzhyi <soulline0@gmail.com>
2023-01-26 17:59:55 +0100

    add private/public route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,15 +3,42 @@ import { SharedLayout } from 'pages/SharedLayout';
 import { Contacts } from 'pages/Contacts';
 import { Register } from './Register/Register';
 import { Login } from './Login/Login';
+import { PublicRoute } from './service/PublicRoute';
+import { PrivateRoute } from './service/PrivateRoute';
+
+// import PrivateRoute from './service/PrivateRoute';
 
 export const App = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<SharedLayout />}>
-          <Route path="register" element={<Register />} />
-          <Route path="login" element={<Login />} />
-          <Route path="contacts" element={<Contacts />} />
+          {/* <Route path="register" element={<Register />} /> */}
+
+          <Route
+            path="register"
+            element={
+              <PublicRoute
+                component={Register}
+                redirectTo="/login"
+                restricted
+              />
+            }
+          />
+          <Route
+            path="login"
+            element={
+              <PublicRoute
+                component={Login}
+                redirectTo="/contacts"
+                restricted
+              />
+            }
+          />
+          <Route
+            path="contacts"
+            element={<PrivateRoute component={Contacts} redirectTo="/login" />}
+          ></Route>
         </Route>
       </Routes>
     </>
---
+++ src/components/service/PublicRoute.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { Navigate } from 'react-router-dom'
+import { selectIsLoggedIn } from 'redux/auth/authSelector'
+
+export const PublicRoute = ({component: Component, redirectTo = "/", restricted = false}) => {
+  
+  const isLoggedIn = useSelector(selectIsLoggedIn)
+  const shoudlRedirect = isLoggedIn && restricted;
+  return (
+  <>
+    {/* {shoudlRedirect ? navigate(redirectTo) : <Component/>} */}
+    {shoudlRedirect ? <Navigate to={redirectTo}/>  : <Component/>}
+  </>
+      )
+}
+
+
+
